2937fe613b9d2f0dcbc797621e4bf8443e53ca60
Suguru Miyata <suguru1114@gmail.com>
2024-06-19 23:09:50 +1000

    add text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 // rfce for getting basic lines of code 
-import React from 'react'  
-// import React, {useState} from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
   return (
     <div>
-      
+      <h1>Todo list</h1>
+      <input   
+        type = "text"
+
+      />
+      <button>Add</button>
+    
     </div>
   )
 }
