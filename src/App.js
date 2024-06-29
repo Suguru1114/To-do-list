74bd6ef38860308b9a71d761efa99804ea1bbf7a
Suguru Miyata <suguru1114@gmail.com>
2024-06-29 17:23:08 +1000

    add use state list but not able to type

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [list, setList] = useState([]);
-  const [input, setInput] = useState("");
+  const [list, setList] = useState([]); // This is for the list of to-dos
+  const [input, setInput] = useState(""); // This is for the current input value
 
-  return (
-    <div>
-      <h1>Todo list</h1>
-      <input   
-        type = "text"
 
-      />
-      <button>Add</button>
-    
-    </div>
-  )
+  function handleInputChange(event){
+    setList(event.target.value)  //i wanna add text
+  }
+  
+  function addTask(){
+    setList([...list, input]);
+    //need to think id the new item was blank or empty
+    }
+
+return (
+  <div>
+    <h1>Todo list</h1>
+    <input type = "text" value ={input} onChange={handleInputChange}/>
+    <button onClick={addTask}>Add</button>
+  
+  </div>
+)
+
 }
-
-export default App
+export default App;
